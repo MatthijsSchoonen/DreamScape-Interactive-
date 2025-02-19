a19b5ac6db442aae654b9e5b2a277e66e78ecee5
Matthijs <121012177+1Matthijs@users.noreply.github.com>
2025-02-19 09:14:22 +0100

    data

---
+++ DreamScape/DreamScape/Model/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamScape.Model
+{
+    class Inventory
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ DreamScape/DreamScape/Model/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamScape.Model
+{
+    class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TypeId { get; set; }
+        public Type Type { get; set; }
+        public int RarityId { get; set; }
+        public Rarity Rarity { get; set; }
+        public int Power { get; set; }
+        public int Speed { get; set; }
+        public int Durability { get; set; }
+        public string Magic { get; set; }
+    }
+}
